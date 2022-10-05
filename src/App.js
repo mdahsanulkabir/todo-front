430dc61e94592c4f059836ca88af06814263abc3
ahsan <makabir98@yahoo.com>
2022-10-05 13:08:21 +0600

    firebase credentials entered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Home from './pages/Home/Home/Home';
+import Footer from './pages/Shared/Footer/Footer';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h3>I am footer</h3>
+        </div>
+    );
+};
+
+export default Footer;
