095c4cbb3e76223ba75db7706469a34217479397
ahsan <makabir98@yahoo.com>
2022-10-05 13:29:03 +0600

    interface with firebase authentication done

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,44 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 const Home = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  if (error) {
     return (
-        <div>
-            <h1>This is home</h1>
-        </div>
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
     );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Signed In User: {user.email}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>This is home</h1>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={() => signInWithGoogle()}>Sign In</button>
+    </div>
+  );
 };
 
 export default Home;
